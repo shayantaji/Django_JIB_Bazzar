7609bc14a4a4d952fa42e448521b86f5ebda611a
shayan <shayanshayanman@gmail.com>
2026-06-06 17:44:13 +0430

    move hexa template to django and make renderpartial_include to them. . .(1)

--- Jib_Bazar/settings.py
+++ Jib_Bazar/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Home_page',
+    'django_render_partial'
 ]
 
 MIDDLEWARE = [
@@ -116,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [ BASE_DIR / 'static' ]
+
