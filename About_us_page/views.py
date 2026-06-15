5976a1af54baec203af38513b5c815df3d05c7df
shayan <shayanshayanman@gmail.com>
2026-06-15 19:55:37 +0430

    dynamic about us and contact us data but nut finished yet  (13)

--- About_us_page/views.py
+++ About_us_page/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-
-
+from site_config.models import SiteSetting
 def About_Us_page(request):
-
+    data=SiteSetting.objects.first()
     context = {
-
+        'site_setting': data,
     }
     return render(request,'about_us/about_us.html',context)
--- Contact_us_page/views.py
+++ Contact_us_page/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 
 from django.shortcuts import render
 
+from site_config.models import SiteSetting
+
 
 def Contact_Us_page(request):
+    data = SiteSetting.objects.first()
     context = {
-
+        'site_setting': data
     }
     return render(request, 'contact_us/contact_us.html', context)
